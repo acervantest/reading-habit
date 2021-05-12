af9b6e0635d4bbdb5e1c02535c6044a0bb9f3580
alejandro <acervantest83@gmail.com>
2021-05-12 12:29:06 -0700

    react-bootstrap library  added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/pages/Home';
 import Navbar from './components/layout/Navbar';
 import UserDetail from './components/users/UserDetail';
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,21 @@
 import React, { Fragment } from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
 import Users from '../users/Users';
 
 const Home = () => (
     <Fragment>
+        <Jumbotron>
+            <h1>Welcome to your personal reading tracker!</h1>
+            <p>
+                This is a simple way to keep track of your reading progress, get statistics on how many books 
+                you are reading every certain time, how many pages you are reading every day and even more important,
+                one simple and fun way to keep you motivated to read daily.
+            </p>
+            <p>
+                <Button variant="primary">Add a new user</Button>
+            </p>
+        </Jumbotron>
         <Users />
     </Fragment>
 )
